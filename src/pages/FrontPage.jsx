b00303168cfeec466eb8a1ed5acfe25e6ef979d5
nerdimite <bhavesh.laddagiri1@gmail.com>
2024-01-03 18:45:28 +0530

    replace button with full card logos

--- src/pages/FrontPage.jsx
+++ src/pages/FrontPage.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../components/ui/button";
+import KafkaLogo from "../components/KafkaLogo";
+import SqlLogo from "../components/SqlLogo";
 import { cn } from "../lib/utils";
 
 const FrontPage = () => {
   const navigate = useNavigate();
 
   const cardClasses =
-    "flex justify-center items-center bg-white transition-all duration-500 ease-in-out overflow-hidden w-full";
+    "flex justify-center items-center bg-white rounded-xl m-4 transition-all duration-500 ease-in-out overflow-hidden w-full hover:cursor-pointer hover:shadow-xl hover:bg-primary hover:text-white hover:fill-white hover:border-2 hover:-translate-y-2 ";
 
   const [expandSide, setExpandSide] = useState(0);
   const [isNavigating, setIsNavigating] = useState(false);
@@ -16,11 +18,6 @@ const FrontPage = () => {
     setExpandSide(side);
     handleNavigation();
   };
-
-  //   const resetSide = (e) => {
-  //     e.preventDefault();
-  //     setExpandSide(0);
-  //   };
 
   const handleNavigation = () => {
     setTimeout(() => {
@@ -35,47 +32,27 @@ const FrontPage = () => {
     <div
       key="1"
       className={cn(
-        "flex h-screen mx-2 gap-2 bg-primary transition-all duration-500",
+        "flex h-screen bg-primary transition-all duration-500",
         isNavigating ? "animate-fadeOut" : ""
       )}
     >
-      <div className={cn(cardClasses, "rounded-r-xl", expandSide === 2 && "w-0")}>
-        <Button
-          className="w-96 bg-purple-600 hover:bg-purple-700 text-white rounded-md py-2"
-          variant="default"
-          onClick={(e) => {
-            e.preventDefault();
-            toggleSide(1);
-          }}
-        >
-          Login to MySQL
-        </Button>
-        {/* <Button
-          variant="destructive"
-          className="w-full mt-2"
-          onClick={resetSide}
-        >
-          Reset
-        </Button> */}
+      <div
+        className={cn(cardClasses, "mr-2", expandSide === 2 && "w-0")}
+        onClick={(e) => {
+          e.preventDefault();
+          toggleSide(1);
+        }}
+      >
+        <SqlLogo />
       </div>
-      <div className={cn(cardClasses, "rounded-l-xl", expandSide === 1 && "w-0")}>
-        <Button
-          className="w-96 bg-pink-600 hover:bg-pink-700 text-white rounded-md py-2"
-          variant="default"
-          onClick={(e) => {
-            e.preventDefault();
-            toggleSide(2);
-          }}
-        >
-          Login to Kafka
-        </Button>
-        {/* <Button
-          variant="destructive"
-          className="w-full mt-2"
-          onClick={resetSide}
-        >
-          Reset
-        </Button> */}
+      <div
+        className={cn(cardClasses, "ml-2", expandSide === 1 && "w-0")}
+        onClick={(e) => {
+          e.preventDefault();
+          toggleSide(2);
+        }}
+      >
+        <KafkaLogo />
       </div>
     </div>
   );
